a06a0f647424afe3fe33734090b43239ade8e76b
Elgin Davis <elgindavis@college.harvard.edu>
2018-05-11 07:30:15 -0500

    initial comit

---
+++ Movement2D.gd
@@ -0,0 +1,39 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+const GRAVITY = 20
+const SPEED = 200
+const JUMP_HEIGHT = -550
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	motion.y += GRAVITY
+
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+		$Sprite.flip_h = false
+		$Sprite.play("run")
+
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+		$Sprite.flip_h = true
+		$Sprite.play("run")
+	else:
+		$Sprite.play("idle")
+		motion.x = 0
+
+
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+	else:
+#	To fly if the up button is held down	
+#		if Input.is_action_pressed("ui_up"):
+#			$Sprite.play("fly")
+#			motion.y = JUMP_HEIGHT / 2
+#		else:
+#			$Sprite.play("run")
+		$Sprite.play("jump")
+
+	motion = move_and_slide(motion, UP)
---
+++ StartMenu.gd
@@ -0,0 +1,18 @@
+#StartMenu.gd
+extends Control
+
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+export(String, FILE, "*.tscn") var next_world
+
+
+func _on_StartGame_pressed():
+	print("Start!")
+	get_tree().change_scene(next_world)
+
+
+func _on_QuitGame_pressed():
+	get_tree().quit()
